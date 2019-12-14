3665c25a5f964d2888565ef6161dbbca75e54ff9
nickr23 <nickrich2624@gmail.com>
2019-12-14 01:09:21 -0500

    Added screen matrix

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 #include <vector>
 using namespace std;
 
-struct coords { int x; int y;};
 
-struct star {
+const auto nx = 1280;
+const auto ny = 1280;
+
+struct Coords { int x; int y;};
+struct Color { int r; int g; int b;};
+struct Star {
 	int x;
 	int y;
 	int size;
 };
 
-bool isInStarMap(int x, int y, vector<star> stars) {
-	for (star currentStar : stars) {
+bool isInStarMap(int x, int y, vector<Star> stars) {
+	for (Star currentStar : stars) {
 		if (currentStar.x == x && currentStar.y == y) {
 			return true;
 		}
@@ -20,42 +24,51 @@ bool isInStarMap(int x, int y, vector<star> stars) {
 	return false;
 }
 
-struct pixel {
-	struct coords point; int color;
-}; 
+void renderStars(Color** screen, vector<Star> stars) {
+	for (Star currentStar: stars) {
+		screen[currentStar.x][currentStar.y] = { 255, 0, 0};
+	}
+}
+
+void drawScreen(Color** screen)	{
+	for (auto r = 0; r < nx; r++){
+		for (auto c = 0; c < ny; c++){
+			Color pixel = screen[r][c];
+			cout << pixel.r << " " << pixel.g << " " << pixel.b << endl;
+		}
+	}
+}
 
 int main(int argc, char** argv) {
 	//Initiallize screen
-	vector<vector<pixel>> screen;	
-	int nx = 1280;
-	int ny = 1280;
-	vector<star> stars;
-	stars.push_back({10,10});		
-	stars.push_back({30, 20});	
+	Color **screen = (Color **) malloc(nx * sizeof(Color *));
+	for (auto i = 0; i < nx; i++) {
+		screen[i] = (Color *)malloc(ny * sizeof(Color));
+	}	
 	
+	vector<Star> stars;
+	stars.push_back({3,4});		
+	stars.push_back({7, 5});	
+	
+//	renderStars(screen, stars);	
 	cout << "P3" << endl;
 	cout << nx << " " << ny << endl;
 	cout << "255" << endl;
-	
+		
+		
 	for (int x = 0; x < nx; x++) {
 		for (int y = 0; y < ny; y++) {
-			
-			if (isInStarMap(x, y, stars)) {
-					cout << 255 << " " << 255 << " " << 255 << endl;	
-			}
-			else{	
-				float cx = float(x) / float(nx);
-				float cy = float(y) / float(ny);
+		
+			float cx = float(x) / float(nx);
+			float cy = float(y) / float(ny);
 
-				cx *= 254.9;
-				cy *= 254.9;
-			
-				cout << 0 << " " << 0 << " " << 0 << endl;	
-				//cout << int(cx) << " ";
-				//cout << int(cy) << " ";
-				//cout << "200" << endl;		
-			}	
+			cx *= 254.9;
+			cy *= 254.9;
+			screen[x][y] = {(int)cx, (int)cy};	
+			//cout << int(cx) << " " << int(cy) << " " << 250 << endl;	
 		}
 	}
+	
+	drawScreen(screen);
 	return 0;
 }
